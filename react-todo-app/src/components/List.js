9773e7e87e38a93198aaa79f00622efdc1c57ad1
unknown <peter001019@naver.com>
2024-01-01 17:21:17 +0900

    컴포넌트 렌더링 최적화

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,43 +1,51 @@
-import React from 'react'
+import React from "react";
 
-export default function List({todoData, setTodoData, id, title, completed, provided, snapshot}) {
-    const handleClick = (id) => {
-        let newTododata = todoData.filter((data) => data.id !== id);
-        setTodoData(newTododata);
-      };
-    
-      const handleCompleteChange = (id) => {
-        let newTododata = todoData.map((data) => {
-          if (data.id === id) {
-            data.completed = !data.completed;
-          }
-          return data;
-        });
-        setTodoData(newTododata);
-      };
+const List = React.memo(({
+  todoData,
+  setTodoData,
+  id,
+  title,
+  completed,
+  provided,
+  snapshot,
+}) => {
+  const handleClick = (id) => {
+    let newTododata = todoData.filter((data) => data.id !== id);
+    setTodoData(newTododata);
+  };
 
-    return (
-        <div
-            key={id}
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-            {...provided.dragHandleProps}
-            className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`
-            }>
-            <div className='items-center'>
-                <input
-                    type="checkbox"
-                    onChange={() => handleCompleteChange(id)}
-                    defaultChecked={false}
-                />{" "}
-                <span className={completed ? "line-through" : undefined}>{title}</span>
-            </div>
-            <div className='items-center'>
-                <button onClick={() => handleClick(id)}>
-                    x
-                </button>
-            </div>
-        </div>
-    )
-}
+  const handleCompleteChange = (id) => {
+    let newTododata = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(newTododata);
+  };
+  return (
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+    >
+      <div className="items-center">
+        <input
+          type="checkbox"
+          onChange={() => handleCompleteChange(id)}
+          defaultChecked={false}
+        />{" "}
+        <span className={completed ? "line-through" : undefined}>{title}</span>
+      </div>
+      <div className="items-center">
+        <button onClick={() => handleClick(id)}>x</button>
+      </div>
+    </div>
+  );
+});
+
+export default List;
