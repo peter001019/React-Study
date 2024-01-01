031d9447fd17b64f5eaad9c7278a97bc9ee337e6
unknown <peter001019@naver.com>
2024-01-01 17:02:00 +0900

    List 컴포넌트 분리

--- react-todo-app/src/components/Lists.js
+++ react-todo-app/src/components/Lists.js
@@ -1,27 +1,13 @@
-import React from 'react'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import List from "./List";
 
 export default function Lists({ todoData, setTodoData }) {
-  const handleClick = (id) => {
-    let newTododata = todoData.filter((data) => data.id !== id);
-    setTodoData(newTododata);
-  };
-
-  const handleCompleteChange = (id) => {
-    let newTododata = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTododata);
-  };
-  
   const handleEnd = (result) => {
     //result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됩니다.
     console.log(result);
     //목적지가 없으면(이벤트 취소) 이 함수를 종료합니다.
-    if(!result.destination) return;
+    if (!result.destination) return;
 
     //리액트 불변성을 지켜주기 위해 새로운 todoData 생성
     const newTododata = todoData;
@@ -38,7 +24,7 @@ export default function Lists({ todoData, setTodoData }) {
   return (
     <div>
       <DragDropContext onDragEnd={handleEnd}>
-        <Droppable droppableId='to-do'>
+        <Droppable droppableId="to-do">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
               {todoData.map((data, index) => (
@@ -48,28 +34,15 @@ export default function Lists({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
-                      key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`
-                    }>
-                      <div className='items-center'>
-                        <input
-                          type="checkbox"
-                          onChange={() => handleCompleteChange(data.id)}
-                          defaultChecked={false}
-                        />{" "}
-                        <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
-                      </div>
-                      <div className='items-center'>
-                        <button onClick={() => handleClick(data.id)}>
-                          x
-                        </button>
-                      </div>
-                    </div>
+                    <List
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      provided={provided}
+                      snapshot={snapshot}
+                    ></List>
                   )}
                 </Draggable>
               ))}
@@ -79,5 +52,5 @@ export default function Lists({ todoData, setTodoData }) {
         </Droppable>
       </DragDropContext>
     </div>
-  )
+  );
 }
