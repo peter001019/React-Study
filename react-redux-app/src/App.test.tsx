7b07160bf942b46c6390d1821fec6a8caad68de5
unknown <peter001019@naver.com>
2024-01-24 18:00:27 +0900

    feat: 기본 구조 설정

--- react-redux-app/src/App.test.tsx
+++ react-redux-app/src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { createStore } from "redux";
+import counter from "./reducers";
 
-test('renders learn react link', () => {
-  render(<App />);
+const store = createStore(counter);
+
+test("renders learn react link", () => {
+  render(
+    <App
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
