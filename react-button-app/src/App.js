c1f2ca865cb0ea77fdc720d3fe202fcbc328dd3b
unknown <peter001019@naver.com>
2024-02-05 23:54:10 +0900

    feat: 버튼 act 테스트 코드 추가

--- react-button-app/src/App.js
+++ react-button-app/src/App.js
@@ -11,7 +11,12 @@ function App() {
         <h3 data-testid="counter">{counter}</h3>
         <div>
           <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter((count) => count + 1)}
+          >
+            +
+          </button>
         </div>
       </header>
     </div>
