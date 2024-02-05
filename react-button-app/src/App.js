116799b14fbedb6f18d8dfc0f4689dcffc9a555a
unknown <peter001019@naver.com>
2024-02-06 00:08:58 +0900

    feat: adding on/off button test code

--- react-button-app/src/App.js
+++ react-button-app/src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
@@ -13,14 +14,27 @@ function App() {
           <button
             data-testid="minus-button"
             onClick={() => setCounter((count) => count - 1)}
+            disabled={disabled}
           >
             -
           </button>
           <button
             data-testid="plus-button"
             onClick={() => setCounter((count) => count + 1)}
+            disabled={disabled}
           >
             +
+          </button>
+        </div>
+        <div>
+          <button
+            style={{ backgroundColor: "blue" }}
+            data-testid="on/off-button"
+            onClick={() => {
+              setDisabled((prev) => !prev);
+            }}
+          >
+            on/off
           </button>
         </div>
       </header>
