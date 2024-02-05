06934130b53c99978ff9d005f7c279f7ad6b6f29
unknown <peter001019@naver.com>
2024-02-05 23:32:47 +0900

    feat: test 코드 작성

--- react-button-app/src/App.test.js
+++ react-button-app/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
+  // App 컴포넌트를 렌더링
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //screen object를 이용해서 원하는 엘리먼트에 접근 (접근할 때 ID로 접근)
+  const counterElement = screen.getByTestId("counter");
+
+  //id가 counter인 엘리먼트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
 });
